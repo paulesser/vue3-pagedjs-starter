7f975e5484014ddc9ecd9da668aac0b0542f235a
Paul Eßer <paul.esser@students.hs-mainz.de>
2023-04-02 14:56:12 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vue3 + Vite + Pagedjs starter
+
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+   <button @click="previewer.preview(config.content, config.stylesheets, config.renderTo)" class="no-print"> Run Pagedjs </button>
+  </div>
+</template>
+<script setup>
+import { Previewer } from 'pagedjs'
+const config = {
+  auto: false,
+  before: undefined,
+  after: undefined,
+  content: undefined,
+  stylesheets: undefined,
+  renderTo: undefined,
+  settings: undefined
+}
+const previewer = new Previewer(config.settings)
+</script>
+<style scoped>
+@media print {
+  .no-print {
+    display: none;
+  }
+}
+
+</style>
